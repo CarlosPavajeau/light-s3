04aeef29df058a4fa71fb09726c75245a8ab69bb
CarlosPavajeau <cpavajea@emeal.nttdata.com>
2026-04-29 19:46:22 -0500

    feat: add gracefully shutdown

--- main.go
+++ main.go
@@ -1,7 +1,14 @@
 package main
 
 import (
+	"context"
+	"errors"
 	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
 
 	"light-s3/internal/awsclient"
 	"light-s3/internal/config"
@@ -30,7 +37,26 @@ func main() {
 	g.POST("/presign/upload", h.UploadURL)
 	g.POST("/presign/download", h.DownloadURL)
 
-	if err := g.Run(cfg.Port); err != nil {
-		log.Fatalf("server: %v", err)
+	srv := &http.Server{
+		Addr:    cfg.Port,
+		Handler: g,
+	}
+
+	go func() {
+		if err := srv.ListenAndServe(); err != nil && !errors.Is(err, http.ErrServerClosed) {
+			log.Fatalf("server: %v", err)
+		}
+	}()
+
+	quit := make(chan os.Signal, 1)
+	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
+	<-quit
+	log.Println("shutting down...")
+
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	if err := srv.Shutdown(ctx); err != nil {
+		log.Fatalf("forced shutdown: %v", err)
 	}
 }
